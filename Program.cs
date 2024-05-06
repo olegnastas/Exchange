12b34202e377b3383ef10ea9e5ec0e24df155283
oleg <draconfantom@gmail.com>
2024-05-06 16:32:26 +0300

    add new changes in the progect

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ namespace lessons_1
                 default:
                     Console.WriteLine("Выбранна неверная операция");
                     Console.WriteLine("попробуйте еще раз");
-                    Console.WriteLine("Введите верную команду");
+                   // Console.WriteLine("Введите верную команду");
                     break;
             }
 
